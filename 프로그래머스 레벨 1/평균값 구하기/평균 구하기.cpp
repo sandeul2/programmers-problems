c752014506d2eb95eb334c096a5a6b3194823a0d
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-10-29 01:24:30 +0900

    Add files via upload

---
+++ 프로그래머스 레벨 1/평균값 구하기/평균 구하기.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> test1;
+vector<int> test2;
+
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    for (int i = 0; i < arr.size(); i++) {
+        answer = answer + arr[i];
+    }
+    return answer / arr.size();
+}
+
+int main(void) {
+
+    test1 = { 1,2,3,4 };
+    test2 = { 5,5 };
+
+
+    cout << solution(test1) << endl;
+    cout << solution(test2) << endl;
+}
