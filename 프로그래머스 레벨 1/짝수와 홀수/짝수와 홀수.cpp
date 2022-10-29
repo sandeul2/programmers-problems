2e0fc739f0ef50904050c91d71f82e219e0bc43b
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-10-29 17:17:44 +0900

    Add files via upload

---
+++ 프로그래머스 레벨 1/짝수와 홀수/짝수와 홀수.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+    if (num == 0 || num % 2 == 0)
+        return "Even";
+    else
+        return "Odd";
+}
+
+int main(void) {
+
+    int test1 = 3;
+    int test2 = 4;
+
+
+    std::cout << solution(test1) << std::endl;
+    std::cout << solution(test2) << std::endl;
+    return 0;
+}
