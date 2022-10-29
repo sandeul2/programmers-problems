bc7847677882e47b26bafc9d278e9216d54c758d
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-10-30 00:11:24 +0900

    Add files via upload

---
+++ 프로그래머스 레벨 1/약수의 합/약수의 합.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int test1;
+int test2;
+
+
+
+int solution(int n) {
+    if (n >= 0 && n <= 3000) {
+        int answer = 0; int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0)
+                sum = sum + i;
+        }
+        return sum;
+    }
+}
+
+
+int main(void) {
+
+    test1 = 12;
+    test2 = 5;
+
+
+    cout << solution(test1) << endl;
+    cout << solution(test2) << endl;
+    return 0;
+}
