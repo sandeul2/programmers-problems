ed5afd54ad227cadcb070bd5e3ada266b5a1806b
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-10-31 00:10:18 +0900

    Add files via upload

---
+++ 프로그래머스 레벨 1/자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int test1;
+int test2;
+
+
+int solution(int n) {
+    int answer = 0; int a;
+    while (n > 0) {
+        a = n % 10;
+        answer = answer + a;
+        n = n / 10;
+    }
+
+    return answer;
+}
+
+
+int main(void) {
+
+    test1 = 123;
+    test2 = 987;
+
+
+    cout << solution(test1) << endl;
+    cout << solution(test2) << endl;
+    return 0;
+}
