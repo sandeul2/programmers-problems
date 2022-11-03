ffbb7c9304345df6736e8fb587e6c7c5d7e0fb74
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-04 01:24:56 +0900

    Add files via upload

---
+++ 프로그래머스 레벨 1/하샤드 수/하샤드 수.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int test1; int test2;
+int test3; int test4;
+
+
+bool solution(int x) {
+    bool answer = true;
+    int has_Temp = x; int has_Sum = 0;
+    while (has_Temp > 0)
+    {
+        has_Sum += has_Temp % 10;
+        has_Temp /= 10;
+    }
+
+    return x % has_Sum == 0 ? true : false;
+}
+
+int main(void) {
+
+    test1 = 10;
+    test2 = 12;
+    test3 = 11;
+    test4 = 13;
+
+    cout << solution(test1) << endl;
+    cout << solution(test2) << endl;
+    cout << solution(test3) << endl;
+    cout << solution(test4) << endl;
+}
