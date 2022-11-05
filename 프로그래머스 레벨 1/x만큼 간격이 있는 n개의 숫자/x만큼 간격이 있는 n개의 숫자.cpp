06538207c71594abdae5bd201bf5a7ac313ab963
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-06 00:37:27 +0900

    Add files via upload

---
+++ 프로그래머스 레벨 1/x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int x; int n;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer; int array = 0;
+    for (int i = 1; i <= n; i++) {
+        array = i * x;
+        answer.push_back(array);
+    }
+    return answer;
+}
+
+
+int main(void) {
+
+    x = 2;
+    n = 5;
+
+    vector <long long> arr = solution(x, n);
+
+
+    for (int j = 0; j < arr.size(); j++) {
+        cout << arr[j];
+    }
+    return 0;
+}
