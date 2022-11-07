ee86fe29ff1417377c6aacd5a673641e0613ba97
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-08 00:21:17 +0900

    Add files via upload

---
+++ 프로그래머스 레벨 1/나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int test1; int test2;
+
+int solution(int n) {
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 1) {
+            return i;
+        }
+    }
+}
+
+int main(void) {
+
+    test1 = 10; test2 = 12;
+    cout << solution(test1) << endl;
+    cout << solution(test2) << endl;
+    return 0;
+}
