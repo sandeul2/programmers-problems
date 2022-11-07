6418e8d3a5538c30f137d0d1f98ff114f2198339
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-07 23:09:58 +0900

    Add files via upload

---
+++ 프로그래머스 레벨 1/정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int test1;
+
+long long solution(long long n) {
+    long long answer = 0;
+    string s = to_string(n);
+    for (int i = 0; i < s.size(); i++) {
+        sort(s.rbegin(), s.rend());
+    }
+
+    return stoll(s);
+}
+
+int main(void) {
+
+    test1 = 118372;
+    cout << solution(test1) << endl;
+    return 0;
+}
