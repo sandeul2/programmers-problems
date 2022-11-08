cc66994d8ef563586bf769dd0545e8188bb8c024
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-09 00:10:38 +0900

    [level 1] Title: 음양 더하기, Time: 0.02 ms, Memory: 4.04 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    for(int i=0; i<absolutes.size(); i++){
+        if(signs[i]==false){
+            absolutes[i] *= -1;
+        }
+        answer += absolutes[i];
+    }
+    return answer;
+}
