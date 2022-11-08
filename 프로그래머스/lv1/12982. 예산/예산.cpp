d9078020e8966d264be39627c201be48bdf8aec0
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-08 17:07:33 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.cpp
@@ -0,0 +1,22 @@
+
+#include <string>
+#include <vector>
+#include <algorithm>
+ 
+using namespace std;
+ 
+int solution(vector<int> d, int budget)
+{
+    int answer = 0;
+ 
+    sort(d.begin(), d.end());
+ 
+    for (int i = 0; i < d.size(); i++){
+        budget -= d[i];
+        
+        if (budget  < 0){
+             return answer;
+        }
+        answer++;
+    }
+}
