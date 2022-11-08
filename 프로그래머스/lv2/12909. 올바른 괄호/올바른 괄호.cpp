f56be1ed7482c562f34c6c9d5bcf2f145abdaa05
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-08 15:00:13 +0900

    [level 2] Title: 올바른 괄호, Time: 0.15 ms, Memory: 3.92 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,27 @@
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s){
+    int n = 0;
+    
+    for (int i = 0; i < s.length(); i++) {
+        
+        if(s.front() ==')'){
+            return false;
+        }
+            
+        if (n < 0)
+            return false;
+            
+        if (s[i] == '(')
+            n++;
+            
+        else if (s[i] == ')')
+            n--;
+    }
+    
+        
+    return n == 0;
+}
