bcce3de400523f9adc88e710fb3547b862b2ca1c
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-08 21:50:24 +0900

    [level 2] Title: 짝지어 제거하기, Time: 5.06 ms, Memory: 6.37 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+int solution(string s)
+{
+    stack<char> stack;
+    for (int i = 0; i < s.length(); i++) {
+        if (stack.empty()) {
+            stack.push(s[i]);
+        }
+        else {
+            if (stack.top() == s[i]) {
+                stack.pop();
+                continue;
+            }
+            else {
+                stack.push(s[i]);
+            }
+        }
+    }
+    return stack.empty() ? 1 : 0;
+}
