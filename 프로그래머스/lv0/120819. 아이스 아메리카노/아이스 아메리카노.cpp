aa7c1f0c1e8873dce07f6434a598052e39de4f6f
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-10 16:44:38 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 3.55 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.cpp
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.cpp
@@ -4,13 +4,9 @@
 using namespace std;
 
 vector<int> solution(int money) {
-    vector<int> answer; int ahah = 5500;
-    int count = -1;
-    while(money>=0){
-        money -= ahah;
-            count++;
-    }
-    answer.push_back(count);
-    answer.push_back(money + 5500);
+    vector<int> answer;
+
+    answer.push_back(money / 5500);
+    answer.push_back(money % 5500);
     return answer;
 }
