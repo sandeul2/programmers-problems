fbe7a8d0bf8a33b45a9e5329970be602fee4d0d8
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-10 16:28:56 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    
+    reverse(num_list.begin(), num_list.end());
+    
+    return num_list;
+}
