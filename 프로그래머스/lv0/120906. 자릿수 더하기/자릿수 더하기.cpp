962a9da0a9c872de6fc4ef2264694dcdff99a9f3
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-13 22:03:23 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    while(n>0){
+        answer += n%10;
+        n /=10;
+    }
+    return answer;
+}
