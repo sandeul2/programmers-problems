d96f9c494aecd8b6ee5dcdc35041bdde1008607c
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-14 16:04:15 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    
+    for(int i=0; i<my_string.size(); i++){
+        if(my_string[i] == 'a' || my_string[i] == 'e' || my_string[i] == 'i' 
+           || my_string[i] == 'o' || my_string[i] == 'u')
+            continue;
+        else
+            answer += my_string[i];
+    }
+    return answer;
+}
