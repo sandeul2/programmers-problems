f8c8d6de4fc0316ee9cd26ca302ac69c902eb3ea
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-14 15:36:45 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string rsp) {
+    string answer = "";
+    
+    for(int i=0; i<rsp.size(); i++){
+        if(rsp[i] == '2')
+            answer +='0';
+        else if(rsp[i] == '0')
+            answer+= '5';
+        else if(rsp[i] == '5')
+            answer+= '2';
+    }
+    return answer;
+}
