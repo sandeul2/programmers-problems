bca26337cf43608d3afbf6e498e246d126cc1335
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-17 21:44:38 +0900

    [level 0] Title: 공 던지기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.cpp
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 int solution(vector<int> numbers, int k) {
-    int answer = 0; int sum = 1;
-    for(int i=0; i<k; i++){
-        answer =sum;
-        if(numbers.size()<sum)
-            sum = 1;
-        else if(numbers.size() == sum)
-            sum = 0;
-        sum +=2;
-        }
+    int answer = 0;
+    int ind = 0;
+    while(k>1)
+    {
+        k--;
+        ind += 2;
+        ind %= numbers.size();
+    }
+    answer = numbers[ind];
     return answer;
 }
