fbf77a748433b3acb6c00a26328ae69a7093bb71
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-23 15:40:49 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.cpp
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.cpp
@@ -3,34 +3,22 @@
 
 using namespace std;
 
-int arr[26];
-int check[26];
-
 int solution(vector<string> spell, vector<string> dic) {
     int answer = 2;
-    
-    for(int i=0; i<spell.size(); i++)
-        arr[spell[i][0]-'a']=1;
-    
-    for(int i=0; i<dic.size(); i++){
-        int cnt=0;
-        for(int j=0; j<dic[i].size(); j++){
-            if(arr[dic[i][j]-'a']==1 && check[dic[i][j]-'a']==0) {
-                check[dic[i][j]-'a']=1;
-                cnt++;
+
+    for(const auto& v : dic){
+        bool test = true;
+        for(const auto& t : spell){
+            if(v.find(t) == string::npos){
+                test= false;
+                break;
             }
-            
-            else break;
         }
-        
-        if(cnt==spell.size()){
-            answer=1;
+
+        if(test){
+            answer = 1;
             break;
         }
-        
-        for(int j=0; j<26; j++)
-            check[j]=0;
     }
-    
     return answer;
 }
