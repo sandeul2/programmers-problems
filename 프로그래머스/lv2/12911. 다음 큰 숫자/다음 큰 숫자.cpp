61b05edbf52774ca5b6aa110cc85c50ec28eff2b
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-23 20:11:59 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.98 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int toBinary(int n){
+    string r; int count = 0;
+    while (n != 0) {
+        r += (n % 2 == 0 ? "0" : "1");
+        n /= 2;
+    }
+    for(int i=0; i<r.size(); i++){
+        if(r[i] == '1')
+            count++;
+    }
+    return count;
+}
+
+int solution(int n) {
+    int count1 = toBinary(n); int count2 = 0;
+    
+    while(count1 != count2){
+        n++;
+        count2 = toBinary(n);
+    }
+    return n;
+}
