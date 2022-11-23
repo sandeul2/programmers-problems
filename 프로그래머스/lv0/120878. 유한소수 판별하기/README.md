42be9913198ee5b1be808d8596800ba7196e66ca
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-24 02:53:51 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/README.md
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 유한소수 판별하기 - 120878 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120878) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120878#) 
 
 ### 성능 요약
 
-메모리: 4.19 MB, 시간: 0.01 ms
+메모리: 4.13 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.cpp
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.cpp
@@ -3,23 +3,13 @@
 
 using namespace std;
 
-int gcd(int a, int b){
-    if(a%b==0) return b;
-    return gcd(b, a%b);
-}
-
 int solution(int a, int b) {
     int answer = 0;
-    b/=gcd(b, a);
-    
-    while(1){
-        if(b%2==0) b/=2;
-        else if(b%5==0) b/=5;
-        else break;
+    while(b % 2 == 0){
+        b /= 2;
     }
-    
-    if(b==1) answer=1;
-    else answer=2;
-    
-    return answer;
+    while(b % 5 == 0){
+        b /= 5;
+    }
+    return (a % b == 0)?1:2;
 }
