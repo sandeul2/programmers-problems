3b51c58d55bcfd038f3879f98aee162a69adfd2b
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-27 15:29:58 +0900

    [level 0] Title: 로그인 성공?, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> id_pw, vector<vector<string>> db) {
+    string answer = "login";
+    for(int i=0; i<db.size(); i++){
+        for(int j=0; j<db[i].size(); j++){
+            if(id_pw[0]==db[i][0]){
+                if(id_pw[1]==db[i][1]){
+                    return "login";
+                }
+                else
+                    return "wrong pw";
+            }
+            else
+                answer = "fail";
+        }
+    }
+    return answer;
+}
