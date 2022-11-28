1f42e00033893945fd27b136ba8db9583b072429
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-11-28 17:17:47 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.cpp
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 int solution(int n) {
-    int answer = 0;
-    for(int i=1; i<=n; i++){
-        if(i%3==0 || i%10==3 || i/10==3 || i/10==13){
+    
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0 || i % 10 == 3 || (i / 10) % 10 == 3)
             n++;
         }
+     return n;
     }
-    return n;
-}
+
