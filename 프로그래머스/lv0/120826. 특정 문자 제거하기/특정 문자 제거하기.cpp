0babccf0e0f92052ad73117b590b9bebe2979b0d
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2022-12-18 01:24:16 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 4.09 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
@@ -7,7 +7,8 @@ string solution(string my_string, string letter) {
     string answer = "";
     
     for(int i=0; i<my_string.size(); i++)
-        if(my_string[i]!=letter[0]) answer+=my_string[i];
+        if(my_string[i]!=letter[0]) 
+            answer+=my_string[i];
     
     return answer;
 }
