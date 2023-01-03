47bf1f532d5291ececaa272bf088235113b3b54b
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2023-01-04 01:03:30 +0900

    [level 0] Title: 편지, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* message) {
+    int answer = 0;
+    answer = strlen(message) * 2;
+    return answer;
+}
