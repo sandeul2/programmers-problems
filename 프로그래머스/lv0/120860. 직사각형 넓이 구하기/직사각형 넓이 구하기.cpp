9efd97548fd96c79c78644f4cb741f3e6544bea3
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2023-01-16 01:07:08 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.cpp
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.cpp
@@ -7,7 +7,7 @@ int solution(vector<vector<int>> dots) {
     int answer = 0; int nopi = 0; int nubi = 0;
     
     for(int i=0; i<dots.size(); i++){
-        for(int j=i+1; j<dots.size(); j++){
+        for(int j=i+1; j<dots.size()-1; j++){
             if(dots[i][0] == dots[j][0])
                 nopi = abs(dots[i][1] - dots[j][1]);
             if(dots[i][1] == dots[j][1])
