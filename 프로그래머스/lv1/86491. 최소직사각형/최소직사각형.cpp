1c8de3f2801443c5c298284d4e48b7921af83fa0
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2023-01-18 00:01:34 +0900

    [level 1] Title: 최소직사각형, Time: 0.80 ms, Memory: 5.33 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int answer = 0; vector<int> wallet; vector<int> answer_arr; 
+    int right_max = 0; int left_max = 0;
+    
+    for(int i=0; i<sizes.size(); i++){
+       
+        if(sizes[i][0] < sizes[i][1]){
+            reverse(sizes[i].begin(), sizes[i].end());
+        }
+        
+        if(right_max < sizes[i][0]){
+           right_max = sizes[i][0]; 
+        }
+        
+        if(left_max < sizes[i][1]){
+           left_max = sizes[i][1]; 
+        }
+        }
+    
+    for(int j=0; j<sizes.size(); j++){
+        answer_arr.push_back(right_max * left_max);
+    }
+    sort(answer_arr.begin(), answer_arr.end());   
+    return answer_arr[0];
+    }
+      
