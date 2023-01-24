7b5af794a69b403d47bf310808ae7917833bf3fb
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2023-01-24 14:21:33 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.01 ms, Memory: 4.08 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int num1, int num2) {
+    char temp = my_string[num1];
+    my_string[num1] = my_string[num2];
+    my_string[num2] = temp;
+    
+    return my_string;
+}
