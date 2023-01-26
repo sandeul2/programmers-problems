d8f71b989f5d99502bfdfe08ee3980c357f44e92
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2023-01-26 14:44:04 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    bool answer = true;
+    if(s.size() == 4 || s.size() == 6){
+     
+    for(int i=0; i<s.size(); i++){
+        if(s[i]<'0' || s[i]>'9'){
+            answer = false;
+        }
+    }  
+    }
+    else{
+        answer = false;
+    }
+    return answer;
+}
