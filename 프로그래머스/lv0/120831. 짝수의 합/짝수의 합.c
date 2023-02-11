bce1f75bca5d6c68db0e772440d3b4187fd04996
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2023-02-11 12:41:07 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    for(int i=0; i<=n; i++){
+        if(i%2==0)
+            answer = answer + i;
+    }
+    return answer;
+}
