6228f12e0dafe242505eee3d618675b40d741bb7
popiiy53 <107553545+popiiy53@users.noreply.github.com>
2023-02-13 15:41:38 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/README.md
+++ 프로그래머스/lv0/120890. 가까운 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 가까운 수 - 120890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890) 
 
 ### 성능 요약
 
-메모리: 3.66 MB, 시간: 0.01 ms
+메모리: 4.14 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.cpp
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.cpp
@@ -5,11 +5,13 @@ using namespace std;
 
 int solution(vector<int> array, int n) {
     sort(array.begin(), array.end());
-    int new_Arr = abs(array[0] - n);
+    
+    int new_arr = abs(array[0] - n);
     int index = 0;
+    
     for(int i = 1; i<array.size(); i++){
-        if(new_Arr > abs(array[i] - n)) {
-            new_Arr = abs(array[i] - n);
+        if(new_arr > abs(array[i] - n)) {
+            new_arr = abs(array[i] - n);
             index = i;
         }
     }
