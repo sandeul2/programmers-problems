2af26a2533ad96816f0dde6c8f8e61e1486234cf
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-08-24 23:08:57 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/README.md
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 다루기 기본 - 12918 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12918) 
 
 ### 성능 요약
 
-메모리: 4.17 MB, 시간: 0.01 ms
+메모리: 4.02 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -5,16 +5,13 @@ using namespace std;
 
 bool solution(string s) {
     bool answer = true;
-    if(s.size() == 4 || s.size() == 6){
-     
-    for(int i=0; i<s.size(); i++){
-        if(s[i]<'0' || s[i]>'9'){
-            answer = false;
-        }
-    }  
-    }
-    else{
+    if(s.size() != 4 && s.size() != 6) {
         answer = false;
+    }
+    
+    for(int i = 0; i < s.size(); i++) {
+        if(s[i] < '0' || s[i] > '9') 
+            answer = false;
     }
     return answer;
 }
