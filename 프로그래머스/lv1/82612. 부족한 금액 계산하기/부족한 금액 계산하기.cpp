eb5491d50c162f99228fa751f0b6ece71742c3b5
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-08-28 00:24:52 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cpp
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cpp
@@ -3,10 +3,11 @@ using namespace std;
 long long solution(int price, int money, int count){
     long long answer = money;
     for(int i=1; i<=count; i++){
-        answer -= price * i;
+        answer -= (price * i);
     }
     if(answer>=0){
         return 0;
     }
-    return answer*-1;
+    
+    return answer * -1;
 }
