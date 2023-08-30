a3ca0e94890506fbcd149f5bb251687ae77412d5
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-08-30 15:00:30 +0900

    [unrated] Title: 덧칠하기, Time: 0.16 ms, Memory: 4.98 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.cpp
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.cpp
@@ -1,14 +1,16 @@
 #include <string>
 #include <vector>
+
 using namespace std;
 
-int solution(int n, int m, vector<int> section){
-    int answer = 0, cur = 0;
-    for(int i=0;i<section.size();i++){
-        if (cur > section[i]) 
-            continue;// 현재 칠해진부분이 타겟부분보다 클때
-        answer++;
-        cur = section[i] + m;// 현재 칠해진 부분 갱신
+int solution(int n, int m, vector<int> section) {
+    int answer = 0;
+    int done = 0;
+    for (const auto & a : section) {
+        if (a > done) {
+            done = a + m - 1;
+            answer++;
+        }
     }
     return answer;
 }
