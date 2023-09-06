7be8d0634f920c4a4d59a9170e57bd684ee83caf
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-09-06 16:23:10 +0900

    [unrated] Title: 추억 점수, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.cpp
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.cpp
@@ -1,19 +1,19 @@
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
     vector<int> answer;
-
-    for (int i=0;i<photo.size();i++){
-        int sum=0;
-        for (int j=0;j<photo[i].size();j++){
-            for (int k=0;k<name.size();k++){
-                if(photo[i][j] == name[k]){
-                    sum += yearning[k];
-                }
-            }
+    unordered_map<string, int> m;
+    for (int i = 0; i < name.size(); i++) {
+        m[name[i]] = yearning[i];
+    }
+    for (int i = 0; i < photo.size(); i++) {
+        int sum = 0;
+        for (int j = 0; j < photo[i].size(); j++) {
+            sum += m[photo[i][j]];
         }
         answer.push_back(sum);
     }
