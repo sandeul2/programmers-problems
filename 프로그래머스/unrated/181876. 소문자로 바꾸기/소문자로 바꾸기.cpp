849271f772350b902289adfd199725737faef82c
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-09-12 00:02:02 +0900

    [unrated] Title: 소문자로 바꾸기, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for(int i=0; i<myString.size(); i++){
+        if(myString[i]>='A' && myString[i]<= 'Z')
+            answer += myString[i]+32;
+        else
+            answer +=myString[i];
+    }
+    return answer;
+}
