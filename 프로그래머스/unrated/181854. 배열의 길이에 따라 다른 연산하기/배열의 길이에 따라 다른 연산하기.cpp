5afffe9e64b869b3dd368f7feb3d1edb2be70abc
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-09-15 21:42:13 +0900

    [unrated] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int n) {
+    vector<int> answer;
+    for(int i=0; i<arr.size(); i++){
+        if(arr.size()%2 !=0){
+            if(i%2==0)
+                answer.push_back(arr[i]+n);
+            else
+                answer.push_back(arr[i]);
+        }
+        else{
+            if(i%2!=0)
+                answer.push_back(arr[i]+n);
+            else
+                answer.push_back(arr[i]);
+        }
+            
+    }
+    return answer;
+}
