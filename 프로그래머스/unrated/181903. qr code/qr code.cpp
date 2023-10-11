a4545074a9e9c2ba0685ef22b6982ac1598a023c
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-10-12 00:10:52 +0900

    [level unrated] Title: qr code, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181903. qr code/qr code.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int q, int r, string code) {
+    string answer = "";
+    for(int i=0; i<code.size(); i++){
+        if(i%q == r){
+            answer += code[i];
+        }
+    }
+    return answer;
+}
