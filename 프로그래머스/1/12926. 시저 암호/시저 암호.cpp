2d16eabe63f8149ae3c94628a5f06440854e6a0f
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-10-23 00:26:25 +0900

    [level 1] Title: 시저 암호, Time: 0.06 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(string s, int n) {
+    
+    for(int i=0; i<s.size(); i++){
+        if(s[i]>= 'A' && s[i] <= 'Z'){
+            s[i] = (s[i] - 'A' + n)%26 + 'A';
+        }
+        
+        else if(s[i] >= 'a' && s[i] <= 'z'){
+            s[i] = (s[i] - 'a' + n)%26 + 'a';
+        }
+    }
+    return s;
+}
