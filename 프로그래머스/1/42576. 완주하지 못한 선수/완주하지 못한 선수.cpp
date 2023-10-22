d702e56503a72adff982a053542a5cc45c662d49
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-10-23 00:31:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 34.97 ms, Memory: 32.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -1,21 +1,26 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <unordered_map> //hash table 라이브러리
 
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
-    string answer = ""; int size=participant.size();
-    //1. 참가자와 완주자 정렬
-    sort(participant.begin(), participant.end());
-    sort(completion.begin(), completion.end());
-    //2. 두 배열이 다를때까지 찾기
-    for(int i=0; i<completion.size(); i++){
-        if(participant[i] != completion[i]){
-            answer = participant[i];
-            return answer;
+    string answer = "";
+    
+    unordered_map<string, int> d;
+    for(auto & i : participant){ //참가자의 이름을 담고 1씩 증가
+        d[i]++;
+    }
+    
+    for(auto &i : completion){ // 완주자 내에 참가자 명단이 나오면 1 감소
+        d[i]--;
+    }
+    
+    for(auto &i : d){
+        if(i.second>0){  
+            answer =i.first;
+            break;
         }
     }
-    //3. 같은값을 찾지 못하면 마지막 인덱스값 리턴
-    return participant[size-1];
+    return answer;
 }
