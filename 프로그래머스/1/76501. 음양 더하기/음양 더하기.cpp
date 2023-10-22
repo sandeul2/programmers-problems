a3618a1e975009c0e861424ab39b36fa7e03aadd
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-10-23 00:21:18 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    for(int i=0; i<absolutes.size(); i++){
+        if(signs[i]==true)
+            answer+=absolutes[i];
+        else
+            answer+=absolutes[i]*-1;
+    }
+    return answer;
+}
