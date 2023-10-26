0397069c1b5e264cd6f7d8914a7ca04de3216543
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-10-26 21:56:54 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+ 
+int max_isprime(int a , int b){
+    int A = max(a, b);
+    int B = min(a, b);
+    
+    while(A % B != 0){
+        int R = A % B;
+        A = B;
+        B = R;
+    }
+    return B;
+}
+ 
+// 최소공배수 = 두 수의 곱 / 최대공약수
+int solution(vector<int> arr) {
+    int answer = arr[0];
+    for(int i = 1; i < arr.size(); i++){
+        int max_Isprime = max_isprime(answer, arr[i]);
+        int min_Multi = answer * arr[i] / max_Isprime;
+        answer = min_Multi;
+    }
+    return answer;
+}
