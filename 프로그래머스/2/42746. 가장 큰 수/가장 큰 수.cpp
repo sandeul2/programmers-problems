4460837363fd4fa614d1d0ba21838ab0a74b70f8
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-10-28 00:00:46 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-bool cmp(int a, int b){
+bool cmp(int a, int b){ //정렬의 기준을 설정하는 함수
     string s1 = to_string(a) + to_string(b);
     string s2 = to_string(b) + to_string(a);
     return s1 > s2;
@@ -12,11 +12,10 @@ bool cmp(int a, int b){
 
 string solution(vector<int> numbers) {
     string answer = "";
-    //1. 큰 값으로 정렬하기
-    sort(numbers.begin(), numbers.end(), cmp);
-    //2. numbers배열에서 한개씩 값에 붙이기 반복
-    for(auto& i : numbers)
+    sort(numbers.begin(), numbers.end(), cmp); //1. 큰 값으로 정렬하기
+    
+    for(auto& i : numbers) //2. numbers배열에서 한개씩 값에 붙이기 반복
         answer += to_string(i);
-    //3. 배열의 값이 0일때 가정
-    return answer[0] == '0' ? "0" : answer;
+
+    return answer[0] == '0' ? "0" : answer; //3. 배열의 값이 0일때 가정
 }
