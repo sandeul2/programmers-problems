244f15b7671f6d044f6d2134cb3851396effb8f3
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-11-25 00:25:54 +0900

    [level unrated] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.cpp
+++ 프로그래머스/unrated/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.cpp
@@ -5,15 +5,11 @@ using namespace std;
 
 int solution(vector<vector<string>> board, int h, int w) {
     int answer = 0;
-
-    // 주어진 좌표의 상하좌우를 확인하며 같은 색깔인지 확인
-    string color = board[h][w]; // 주어진 좌표의 색깔
-
-    // 이동할 상하좌우 방향을 정의
-    vector<int> dx = {-1, 1, 0, 0}; // 좌, 우, 상, 하
+    string color = board[h][w]; 
+    
+    vector<int> dx = {-1, 1, 0, 0}; 
     vector<int> dy = {0, 0, -1, 1};
 
-    // 상하좌우를 확인하며 같은 색깔인지 검사
     for (int i = 0; i < 4; i++) {
         int nx = h + dx[i];
         int ny = w + dy[i];
@@ -24,6 +20,8 @@ int solution(vector<vector<string>> board, int h, int w) {
                 answer++;
             }
         }
+        nx = h - dx[i];
+        ny = w - dy[i];
     }
 
     return answer;
