95050ecb69078bf0b38f2f31436215ae22cfa0b9
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-12-27 02:04:50 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    long long number=num;
+    int answer = 0;
+    while(number!=1) {
+        if(answer==500) {
+            return -1;
+        } else if(number%2==0) {
+            number/=2;
+            answer++;
+        } else {
+            number = (number*3)+1;
+            answer++;
+        }
+    }
+    return answer;
+}
