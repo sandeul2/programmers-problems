3bd2749af2f186946ebcafae147b19a95c26698e
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-12-27 01:32:32 +0900

    [level 2] Title: 올바른 괄호, Time: 0.15 ms, Memory: 3.82 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -3,25 +3,19 @@
 
 using namespace std;
 
-bool solution(string s){
+bool solution(string s) {
     int n = 0;
     
     for (int i = 0; i < s.length(); i++) {
         
         if(s.front() ==')'){
             return false;
-        }
-            
-        if (n < 0)
+        } if (n < 0) {
             return false;
-            
-        if (s[i] == '(')
-            n++;
-            
+        } if (s[i] == '(')
+            n++; 
         else if (s[i] == ')')
             n--;
-    }
-    
-        
+    }   
     return n == 0;
 }
