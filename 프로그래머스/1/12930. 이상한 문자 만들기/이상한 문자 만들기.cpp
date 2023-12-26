7782e6173f7cbb0766dbfa0f8a73603b4422b0eb
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-12-27 01:45:07 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -5,18 +5,16 @@ using namespace std;
 
 string solution(string s) {
     string answer = ""; int index = 0;
-    for(int i=0; i<s.length(); i++){
-        if(s[i] == ' '){
+    for(int i=0; i<s.length(); i++) {
+        if(s[i] == ' ') {
             index = 0;
             continue;
         }
         
-        if(index%2==1){
+        if(index%2==1) {
             if(s[i] >= 'A' && s[i] <= 'Z')
             s[i] = s[i]+32;
-        }
-        
-        else{
+        } else {
             if(s[i] >='a' && s[i] <= 'z')
             s[i] = s[i]-32;
         }
