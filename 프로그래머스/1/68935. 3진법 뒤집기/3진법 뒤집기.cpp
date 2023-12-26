3b0a0dff0ea3b94d7af9eb02ec4d75d36ced82eb
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-12-27 01:43:09 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -8,12 +8,13 @@ int solution(int n) {
     int answer = 0;
     int j = 0;
     vector <int> str;
-    while(n!=0){
+    
+    while(n!=0) {
         str.push_back(n % 3);
         n = n / 3;
     }
 
-    for(int i = str.size() -1 ;i>-1;i--){
+    for(int i = str.size() -1 ;i>-1;i--) {
         answer += str[i] * pow(3,j);
         j++;
     }
