f813eaa18ce648c75a6c4e4c2d46834f708cc82c
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-12-27 01:51:17 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.05 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -5,15 +5,16 @@ using namespace std;
 
 int solution(int left, int right) {
     int answer = 0; int temp=0;
-    for(int i=left; i<=right; i++){
+    
+    for(int i=left; i<=right; i++) {
         temp=0;
-        for(int j=1 ;j<=right; j++){
+        for(int j=1 ;j<=right; j++) {
             if(i%j==0)
                 temp++;
-    }
-        if(temp%2==0)
+        }
+        if(temp%2==0){
             answer+=i;
-        else
+        } else
             answer-=i;
     }
     return answer;
