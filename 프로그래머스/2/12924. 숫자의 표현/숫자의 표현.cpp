095ad0ad6a92f3ba20fbc9a5d89972664cc51c7b
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-12-27 01:30:01 +0900

    [level 2] Title: 숫자의 표현, Time: 0.04 ms, Memory: 3.95 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -5,14 +5,13 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0, tmp = 0;
-    for(int i = 1; i <= n; i++){
+    for(int i = 1; i <= n; i++) {
         tmp = i;
-        for(int j = i + 1; j <= n; j++){
+        for(int j = i + 1; j <= n; j++) {
             if(tmp == n) {
                 answer++; 
                 break;
-            }
-            if(tmp + j > n)
+            } if(tmp + j > n)
                 break;
             tmp += j;
         }
