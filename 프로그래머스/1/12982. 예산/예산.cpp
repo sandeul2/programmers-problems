5af3ecf4054b7bbe8e26d67d7c26f953d4e3f3f9
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-12-27 01:42:24 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.cpp
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -7,9 +7,11 @@ using namespace std;
 int solution(vector<int> d, int budget) {
     int answer = 0;
     sort(d.begin(), d.end());
-    for(int i=0; i<d.size(); i++){
+    
+    for(int i=0; i<d.size(); i++) {
         budget -= d[i];
-        if(budget<0){
+        
+        if(budget<0) {
             break;
         }
         answer++;
