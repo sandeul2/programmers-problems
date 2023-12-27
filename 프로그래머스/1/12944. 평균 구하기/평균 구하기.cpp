22f46cccf6037b0179e0a76e21df3a2722d61275
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-12-27 19:19:53 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 double solution(vector<int> arr) {
     double answer = 0;
-    for(int i=0; i<arr.size(); i++){
+    for(int i=0; i<arr.size(); i++) {
         answer += arr[i];
     }
     return answer/arr.size();
