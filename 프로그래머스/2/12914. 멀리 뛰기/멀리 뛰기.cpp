c979fc06b1abc3660da2e702ffa77e47ebb38d06
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-12-27 21:05:00 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -6,10 +6,14 @@ int dp[2001];
  
 long long solution(int n) {
     long long answer = 0;
+    
     dp[1] = 1; //1,2,3,5,8 -> 피보나치
     dp[2] = 2;
-    for(int i=3;i<=n;i++)
-        dp[i] = (dp[i-1] + dp[i-2])%1234567;
+    
+    for(int i=3;i<=n;i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    }
     answer = dp[n];
+    
     return answer;
 }
