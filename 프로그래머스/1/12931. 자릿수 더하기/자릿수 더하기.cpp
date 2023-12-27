9f1737dac97dfd2cfaf78e49507edeef9884fea8
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-12-27 19:17:45 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 using namespace std;
-int solution(int n){
+int solution(int n) {
     int answer = 0;
-    while(n>0){
+    while(n>0) {
         answer += n%10;
         n /= 10;
     }
