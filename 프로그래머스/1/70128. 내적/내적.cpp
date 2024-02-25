3a374889a814d8574c07b81787ffbf9d8bb0d8e7
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2024-02-25 22:33:03 +0900

    [level 1] Title: 내적, Time: 0.02 ms, Memory: 3.76 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.cpp
+++ 프로그래머스/1/70128. 내적/내적.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int solution(vector<int> a, vector<int> b) {
     int answer = 0;
+    /* 1. 배열 a,b의 내적 구하기 */
     for(int i=0; i<a.size(); i++) {
-        answer += a[i]*b[i];
+        answer += a[i] * b[i]; 
     }
-    
     return answer;
 }
