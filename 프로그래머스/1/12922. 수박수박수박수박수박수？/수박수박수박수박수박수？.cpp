e8949939447d8614e714377431b5a6ed77b7ae23
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2024-02-26 23:55:30 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -5,11 +5,12 @@ using namespace std;
 
 string solution(int n) {
     string answer = "";
-    for(int i=0; i<n;i++) {
-        if(i%2==0) {
+    for(int i=0; i<n; i++) {
+        if(i % 2 == 0) {
             answer += "수";
-        } else
+        } else {
             answer += "박";
+        }
     }
     return answer;
 }
