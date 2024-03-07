084e2877f5de35e6efefc3c504126688cb250caa
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2024-03-08 00:58:11 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 113.04 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -3,17 +3,20 @@
 
 using namespace std;
 
-vector<int> solution(vector<int> arr) {
+vector<int> solution(vector<int> arr) 
+{
     vector<int> answer;
-
+    
     for(int i=0; i<arr.size(); i++) {
+        /* 1. answer가 비어있다면 추가 */
         if(answer.empty()) {
             answer.push_back(arr[i]);
+        /* 2. 연속된 숫자 제거 */
         } else {
-            if(answer.back()!=arr[i])
+            if(arr[i] != answer.back()) {
                 answer.push_back(arr[i]);
-          }
+            }
+        }
     }
-
     return answer;
 }
