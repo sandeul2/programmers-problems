c1c4af2bc90d0cc477a4c569ec708aa4e5dfedf3
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2024-03-20 00:10:04 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -3,23 +3,24 @@
 
 using namespace std;
 
-int max_isprime(int a ,int b) {
+/* 1. 최대공약수 */
+int gcd (int a ,int b) {
     int n;
-    while(b!=0){
+    while (b != 0){
         n = a%b;
         a = b;
         b = n;
     }
     return a;
 }
-
-int min_multiple(int a , int b) {
-    return a*b / max_isprime(a,b);
+/* 2. 최소공배수 */
+int lcm (int a, int b) {
+    return (a * b) / gcd(a, b);
 }
 
 vector<int> solution(int n, int m) {
     vector<int> answer;
-    answer.push_back(max_isprime(n,m));
-    answer.push_back(min_multiple(n,m));
+    answer.push_back(gcd(n, m));
+    answer.push_back(lcm(n, m));
     return answer;
 }
